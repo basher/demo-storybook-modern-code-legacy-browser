72541f2a2992873377c0fbf5bbe3f25468de28d3
basher <basher@madasafish.com>
2024-06-16 12:46:20 +0100

    remove web components

--- src/javascript/ui-init.js
+++ src/javascript/ui-init.js
@@ -1,9 +1,5 @@
 import Disclosure from './components/disclosure';
-import WebUIDisclosure from './web-components/webui-disclosure';
 
 export const uiInit = () => {
     Disclosure.start();
-    // Define Web Components
-    !customElements.get('webui-disclosure') &&
-        customElements.define('webui-disclosure', WebUIDisclosure);
 };
