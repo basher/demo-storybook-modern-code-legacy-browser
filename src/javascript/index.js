4d72b81c9ffdc48a6fa56b1aa602e1c3b44dd777
basher <basher@madasafish.com>
2024-07-08 11:50:14 +0100

    WIP - add theme support

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,9 +1,6 @@
 // Import Sass entry file (whitelabel).
 import '../../src/css/index.scss';
 
-// Import Sass theme file(s).
-import '../../src/css/theme1.scss';
-
 // Import config and UI module initialisation.
 import { browserSupportsAllFeatures } from './config/browser-supports-features';
 import { uiInit } from './ui-init';
