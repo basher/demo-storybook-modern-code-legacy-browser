90a308f12adc0b83ef7e637dee66e7b3268c2c20
basher <basher@madasafish.com>
2024-07-16 10:37:16 +0100

    add console log to  bundle-all-themes

--- scripts/bundle-all-themes.js
+++ scripts/bundle-all-themes.js
@@ -10,6 +10,7 @@ const themes = require('./theme-config');
 let strNpm = '';
 for (const [key] of Object.entries(themes)) {
     strNpm += `npm run build:theme --theme=${key} && `;
+    console.log(`Building UI theme: ${key}`);
 }
 
 strNpm = strNpm.substring(0, strNpm.lastIndexOf('&&') - 1);
